91e8dbea8d982155cf329ed99cd489a95c9a5d8b
Todd Fleeman <rtoddf@comcast.net>
2018-07-21 21:24:04 -0400

    continuing with exercises

--- PizzaHistoryMap/PizzaHistoryMap/ViewController.swift
+++ PizzaHistoryMap/PizzaHistoryMap/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UIViewController {
     
     //MARK: - Properties and outlets
     var coordinate2D = CLLocationCoordinate2DMake(48.8606105, 2.3376447)
+    var camera = MKMapCamera()
+    var pitch = 0
     
     //MARK: Outlets
     @IBOutlet weak var changeMapType: UIButton!
@@ -33,44 +35,62 @@ class ViewController: UIViewController {
     
     //MARK: - Actions
     @IBAction func changeMapType(_ sender: UIButton) {
+        
     }
     
     @IBAction func changePitch(_ sender: UIButton) {
-       
+       pitch  = (pitch + 15) % 90
+        // degrees is option 0
+        sender.setTitle("\(pitch)º", for: .normal)
+        mapView.camera.pitch = CGFloat(pitch)
     }
+    
     @IBAction func toggleMapFeatures(_ sender: UIButton) {
+        
     }
     
     @IBAction func findHere(_ sender: UIButton) {
+        
     }
     
     @IBAction func findPizza(_ sender: UIButton) {
+        
     }
 
     @IBAction func locationPicker(_ sender: UISegmentedControl) {
-        print("click")
         let index = sender.selectedSegmentIndex
         
         switch index {
         case 0:
             // the louvre
-           coordinate2D = CLLocationCoordinate2DMake(48.8606105, 2.3376447)
+            coordinate2D = CLLocationCoordinate2DMake(48.8606105, 2.3376447)
+            updateMapCamera(heading: 245.0, altitude: 1000.0)
+            return
         case 1:
             //  The British Museum In London
             coordinate2D = CLLocationCoordinate2DMake(51.5194537, -0.1268876)
+            updateMapCamera(heading: 245.0, altitude: 1000.0)
+            // the return is so the updateMapRegion function doesn't execute
+            return
         case 2:
             //  The Metropolitan Museum Of Art In New York City
             coordinate2D = CLLocationCoordinate2DMake(40.7794366, -73.963244)
+            updateMapCamera(heading: 12.0, altitude: 1000.0)
+            return
         case 3:
             //  The Art Institute of Chicago
             coordinate2D = CLLocationCoordinate2DMake(41.8796405, -87.6237068)
+            updateMapCamera(heading: 180.0, altitude: 1000.0)
+            return
         case 4:
             //  The Vasa Museum In Stockholm
             coordinate2D = CLLocationCoordinate2DMake(59.3280132, 18.0914612)
+            updateMapCamera(heading: 245.0, altitude: 1000.0)
+            return
         default:
-           coordinate2D = CLLocationCoordinate2DMake(48.8606105, 2.3376447)
+            coordinate2D = CLLocationCoordinate2DMake(48.8606105, 2.3376447)
         }
-        print("\(coordinate2D)")
+        
         updateMapRegion(rangeSpan: 100)
     }
     
@@ -78,6 +98,15 @@ class ViewController: UIViewController {
     func updateMapRegion(rangeSpan:CLLocationDistance) {
         let region = MKCoordinateRegionMakeWithDistance(coordinate2D, rangeSpan, rangeSpan)
         mapView.region = region
+    }
+    
+    func updateMapCamera(heading:CLLocationDirection, altitude:CLLocationDistance) {
+        camera.centerCoordinate = coordinate2D
+        camera.heading = heading
+        camera.altitude = altitude
+        camera.pitch = 0.0
+        changePitch.setTitle("0º", for: .normal)
+        mapView.camera = camera
     }
     
     //MARK: Map setup
